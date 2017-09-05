8b609c143fd2921aca2fd49556e42e4638c3f945
nolte <nolte@ingo.de>
2017-09-05 21:56:36 +0200

    split site prepare and herku prepare

--- scripts/prepare_heroku_deployment.sh
+++ scripts/prepare_heroku_deployment.sh
@@ -3,27 +3,14 @@
 # Create a new Brach for the Heroku App, base on php
 #
 
-mkdir -p $HEROKU_TMP
-# copy generated files to tmp folder
-cp -r $(pwd)/_site/* $HEROKU_TMP
-# copy the tempalte index php required by Heroku
-cp $(pwd)/index.php $HEROKU_TMP
-# save the Procfile for web: heroku-php-apache2
-cp $(pwd)/Procfile $HEROKU_TMP
-# add a empty composer file
-echo "{}" > $HEROKU_TMP/composer.json
+git checkout $TRAVIS_BRANCH -- Procfile
+git checkout $TRAVIS_BRANCH -- index.php
+echo "{}" > composer.json
+
 # create the tmpbrach
 git branch herokuapp
 git checkout herokuapp
-# remove all existing files
-rm -r $(pwd)/*
-rm $(pwd)/.gitignore
-rm -r $(pwd)/.sass-cache
-rm -r $(pwd)/.travis.yml
-
-# copy back the generated files
-cp -r $HEROKU_TMP/* $(pwd)
 
 # add all generated files to the branch
 git add --all
-git commit -am "travisBuild"
+git commit -am "add heroku specific files"
