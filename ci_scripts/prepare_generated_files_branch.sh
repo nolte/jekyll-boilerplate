e926dd33681076f8deac4e55f6e807f0cce4058c
nolte <nolte@ingo.de>
2017-09-06 21:06:26 +0200

    debugs

--- ci_scripts/prepare_generated_files_branch.sh
+++ ci_scripts/prepare_generated_files_branch.sh
@@ -6,6 +6,8 @@ mkdir -p $HEROKU_TMP
 # copy generated files to tmp folder
 cp -r $TRAVIS_BUILD_DIR/jekyll_content/_site/* $HEROKU_TMP
 
+ls -all $HEROKU_TMP
+
 # create the tmpbrach
 git branch $GENERATED_FILE_BRANCH_NAME
 git checkout $GENERATED_FILE_BRANCH_NAME
@@ -16,10 +18,16 @@ ls -all $TRAVIS_BUILD_DIR
 rm -r $TRAVIS_BUILD_DIR/*
 ## remove invisible files
 
-rm -r $TRAVIS_BUILD_DIR/.travis.yml
+ls -all $TRAVIS_BUILD_DIR
 
+rm -r $TRAVIS_BUILD_DIR\/.travis.yml
+ls -all $TRAVIS_BUILD_DIR
+
+echo "copy"
 # copy back the generated files
-cp -r $HEROKU_TMP/* $TRAVIS_BUILD_DIR
+cp -r $HEROKU_TMP\/* $TRAVIS_BUILD_DIR
+echo "copy"
+ls -all $TRAVIS_BUILD_DIR
 
 # add all generated files to the branch
 git add --all
