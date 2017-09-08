c48aee2f21c05e545faaacc289adb0c7fa9da54d
nolte <nolte@ingo.de>
2017-09-08 19:19:34 +0200

    updte deploy script

--- ci_scripts/deploy_ghpage.sh
+++ ci_scripts/deploy_ghpage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -o errexit
-echo $GENERATED_FILE_BRANCH_DIR
 GITHUB_PAGE_DIR=/tmp/ghpage
 
 # config
@@ -8,26 +7,16 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 
-git clone -b gh-pages https://github.com/nolte/jekyll-boilerplate.git $GITHUB_PAGE_DIR
-
+git clone -b gh-pages https://github.com/$TRAVIS_REPO_SLUG.git $GITHUB_PAGE_DIR
 cd $GITHUB_PAGE_DIR
-ls -all $GITHUB_PAGE_DIR
-echo $(pwd)
 
 git rm -rf .
 git clean -fxd
-echo "content after clean"
-echo $(pwd)
-ls -all
-
-
-ls -all $GITHUB_PAGE_DIR
 
 cp -r $GENERATED_FILE_BRANCH_DIR\/* $GITHUB_PAGE_DIR
-
-ls -all
 
 # add all generated files to the branch
 git add --all
 git commit -am $GENERATED_FILE_BRANCH_COMMIT_MSG
 echo "branch with generated files created"
+#git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
