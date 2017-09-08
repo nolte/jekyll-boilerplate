a060852e033583bf500bf56546add783abfaa665
nolte <nolte@ingo.de>
2017-09-08 18:07:48 +0200

    copy files

--- ci_scripts/deploy_ghpage.sh
+++ ci_scripts/deploy_ghpage.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 set -o errexit
 
+GITHUB_PAGE_DIR=/tmp/ghpage
+
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
 
-git clone -b gh-pages https://github.com/nolte/jekyll-boilerplate.git /tmp/ghpage
+git clone -b gh-pages https://github.com/nolte/jekyll-boilerplate.git $GITHUB_PAGE_DIR
 
-
+cd $GITHUB_PAGE_DIR
 ls -all /tmp/ghpage
+find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+
+
+cp -r $HEROKU_TMP\/* $GITHUB_PAGE_DIR
+
+# add all generated files to the branch
+git add --all
+git commit -am $GENERATED_FILE_BRANCH_COMMIT_MSG
+echo "branch with generated files created"
