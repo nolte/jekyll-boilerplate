c9adc275249ed80eeea1140d1aa558f18e2c69fc
nolte <nolte@ingo.de>
2017-09-09 13:15:45 +0200

    other script

--- ci_scripts/deploy_ghpage.sh
+++ ci_scripts/deploy_ghpage.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-set -o errexit
+echo "Starting deployment"
+echo "Target: gh-pages branch"
+
 GITHUB_PAGE_DIR=/tmp/ghpage
+DIST_DIRECTORY=$GITHUB_PAGE_DIR
+CURRENT_COMMIT=`git rev-parse HEAD`
+ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-# config
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
-
-
-git clone -b gh-pages https://github.com/$TRAVIS_REPO_SLUG.git $GITHUB_PAGE_DIR
+#cp .gitignore $DIST_DIRECTORY || exit 1
 cd $GITHUB_PAGE_DIR
+echo "Checking out gh-pages branch"
+git checkout -B gh-pages || exit 1
 
-git rm -rf .
-git clean -fxd
+echo "Removing old static content"
+git rm -rf . || exit 1
 
-cp -r $GENERATED_FILE_BRANCH_DIR\/* $GITHUB_PAGE_DIR
+echo "Copying dist content to root"
 
-# add all generated files to the branch
-git add --all
-git commit -am $GENERATED_FILE_BRANCH_COMMIT_MSG
-echo "branch with generated files created"
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages:gh-pages # > /dev/null 2>&1
+cp -r $GENERATED_FILE_BRANCH_DIR\/* $GITHUB_PAGE_DIR || exit 1
+
+echo "Pushing new content to $ORIGIN_URL"
+git config user.name "YOUR-GITHUB-USERNAME-HERE" || exit 1
+git config user.email "YOUR-GITHUB-EMAIL-HERE" || exit 1
+
+git add -A . || exit 1
+git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+
+echo "Cleaning up temp files"
+rm -Rf $DIST_DIRECTORY
+
+echo "Deployed successfully."
+exit 0
